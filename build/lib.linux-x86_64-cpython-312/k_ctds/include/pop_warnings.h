b9aed1206949d8ef3aaec4107b1814011ed1e2d6
Chad Ongstad <chad@kodda.io>
2026-02-17 13:56:44 +0000

    Rebuild docs for v2.1.0

--- build/lib.linux-x86_64-cpython-312/k_ctds/include/pop_warnings.h
+++
@@ -1,7 +0,0 @@
-#if defined(__GNUC__) && (__GNUC__ > 4)
-#  pragma GCC diagnostic pop
-#endif /* if defined(__GNUC__) */
-
-#if defined(_MSC_VER)
-#  pragma warning(pop)
-#endif /* if defined(_MSC_VER) */
